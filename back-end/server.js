c2a628e9dfcdfe4db2447320a30a224b85fa67f8
ayush181000 <ayush181000@gmail.com>
2021-10-17 04:02:40 +0530

    Mongo DB connected

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -23,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server runnning in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server runnning in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
