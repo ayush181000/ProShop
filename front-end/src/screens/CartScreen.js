1b8a9fd198f2d7dfd4ba6a3ff76edbc3c4de59d4
ayush181000 <ayush181000@gmail.com>
2021-10-20 08:32:27 +0530

    Remove from cart

--- front-end/src/screens/CartScreen.js
+++ front-end/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Card,
 } from 'react-bootstrap';
 import Message from '../components/Message.js';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -31,8 +31,8 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty]);
 
-  const removeFromCartHandler = () => {
-    console.log('remove');
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
   };
   const checkoutHandler = () => {
     history.push('/login?redirect=shipping');
