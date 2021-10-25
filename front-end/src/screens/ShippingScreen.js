32eddc29fe6cb3e7062f8a7e6db3e6db374217a0
ayush181000 <ayush181000@gmail.com>
2021-10-25 14:48:37 +0530

    Payment Method Screen

--- front-end/src/screens/ShippingScreen.js
+++ front-end/src/screens/ShippingScreen.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
-import { saveShippingAddress } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { saveShippingAddress } from '../actions/cartActions';
 
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
