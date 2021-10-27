6425b0f19d1742d40e18e7db8591917f3637dec1
ayush181000 <ayush181000@gmail.com>
2021-10-27 13:45:36 +0530

    Add backend and redux state for order details

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers.js';
-import { orderCreateReducer } from './reducers/orderReducers.js';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
