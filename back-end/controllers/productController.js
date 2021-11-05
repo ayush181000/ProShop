100be1f8f1e262c0437e136902f5016f3a132dc7
ayush181000 <ayush181000@gmail.com>
2021-11-06 03:24:52 +0530

    Review adding functionality backend

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -61,19 +61,11 @@ const createProduct = asyncHandler(async (req, res) => {
 });
 
 // @desc        Update a product
-// @route       POST /products/:id
+// @route       PUT /products/:id
 // @access      Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
-  const {
-    name,
-    price,
-    image,
-    brand,
-    category,
-    countInStock,
-    numReviews,
-    description,
-  } = req.body;
+  const { name, price, image, brand, category, countInStock, description } =
+    req.body;
 
   const product = await Product.findById(req.params.id);
 
@@ -94,10 +86,53 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc        Create a new review
+// @route       POST /products/:id/reviews
+// @access      Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ mesage: 'Review Added' });
+  } else {
+    res.status(400);
+    throw new Error('Product not found');
+  }
+});
+
 export {
   getProductById,
   getProducts,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -11,8 +12,14 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
+
 connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json()); // Body parser
 
